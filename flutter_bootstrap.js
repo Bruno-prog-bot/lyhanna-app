945dd3acfc96e0e8dc62096f265320163f6fcf35
Bruno <chevresdesquatrepilas@gmail.com>
2026-09-01 01:45:19 +0200

    Lyhanna web v1.34.0+2150b — moniteur sans tooltips (bulles collées)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -54,6 +54,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: "polices/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "783942936" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "3849582724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
