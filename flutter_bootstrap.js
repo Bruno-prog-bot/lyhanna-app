528b0842cb05abf7324bc8c48c8bfd97924b7c84
Bruno <chevresdesquatrepilas@gmail.com>
2026-09-01 23:57:23 +0200

    Lyhanna web v1.39.1+2156 — mention tutelle majeurs + icône contacts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -54,6 +54,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: "polices/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "924641175" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "3504411004" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
