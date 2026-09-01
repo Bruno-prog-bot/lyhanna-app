0c1226531cc3ec7d691e2a54b23cf4927b11fd12
Bruno <chevresdesquatrepilas@gmail.com>
2026-09-01 18:49:43 +0200

    Lyhanna web v1.38.0+2154 — bulles d'aide + guide testeurs (/guide.html)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -54,6 +54,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: "polices/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3664214019" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "3633313555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
