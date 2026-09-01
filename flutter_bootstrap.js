289b730f51d66bbb353405c0bc5533c1c6915bfa
Bruno <chevresdesquatrepilas@gmail.com>
2026-09-01 23:28:48 +0200

    Lyhanna web v1.39.0+2155 — 0213 : liaison de compte, copier code/lien, push réparé

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -54,6 +54,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: "polices/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3633313555" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "924641175" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
