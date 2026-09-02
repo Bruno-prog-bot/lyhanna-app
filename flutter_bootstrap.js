40246c26be1142c122cbf46d10bd64a24de0af49
Bruno <chevresdesquatrepilas@gmail.com>
2026-09-03 00:17:28 +0200

    Lyhanna web v1.40.1+2158 — télémétrie bêta + carte Mon téléphone

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -54,6 +54,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: "polices/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4290138549" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "1833176132" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
