6e4a6c635353dfd310fb1b5ca9079da83635b759
Bruno <chevresdesquatrepilas@gmail.com>
2026-09-08 22:39:24 +0200

    Lyhanna web v174 — version gravee : le PC sait enfin quelle version il execute

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -54,6 +54,6 @@ _flutter.loader.load({
     fontFallbackBaseUrl: "polices/",
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1558969462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2352572892" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
